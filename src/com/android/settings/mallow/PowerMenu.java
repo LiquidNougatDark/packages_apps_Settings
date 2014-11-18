1978068a6830f65a174fccdbbea8811669f77b70
Evisceration <eviscerationls@gmail.com>
2014-11-18 20:17:17 +0100

    Settings: instant reboot action trigger (2/2)
    Change-Id: I81926d619a24fe004858b4cfc15bd34c5d7adae2
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- src/com/android/settings/mallow/PowerMenu.java
+++ src/com/android/settings/mallow/PowerMenu.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.screwd;
+package com.android.settings.mallow;
 
 import android.content.Context;
 import android.content.ContentResolver;
