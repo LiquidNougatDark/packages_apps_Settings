dfecb37a329c8d7de95279cdf5f64cc0edd29b84
SpiritCroc <spiritcroc@gmail.com>
2015-08-29 12:05:25 +0200

    [2/2] Slim Recent App Sidebar
    One thing I was missing with SlimRecents was the possibility to launch your favorite apps from the recents screen. As SlimRecents covers only the half of the screen (more or less), there is enough space on the other edge to display an app sidebar, which I based on the ChameleonOS one.
    
    Changes to the ChameleonOS sidebar include:
    - No trigger areas, but show/hide with the SlimRecent screen
    - Adapted look and animation to SlimRecent
    - Longpress actions
    - Slimer settings using ActionListViewSettings, also added possibility to configure sidebar size (roughly, as the size is adapted to the screen height in order to show full icons) and colors
    - Currently no option to add folders to the sidebar, as this does not work with the current ActionListViewSettings without further work
    
    Change-Id: I3acc2327dce3aae01d5ae722204639b213c5f750
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- src/com/android/settings/mallow/NavigationBar.java
+++ src/com/android/settings/mallow/NavigationBar.java
@@ -161,7 +161,12 @@ public class NavigationBar extends SettingsPreferenceFragment
 
         updateSettings();
     }
-	
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.DONT_TRACK_ME_BRO;
+    }
+
 	private void updateSettings() {
         boolean enableNavigationBar = Action.isNavBarEnabled(getActivity());
 
@@ -218,11 +223,6 @@ public class NavigationBar extends SettingsPreferenceFragment
         mDimNavButtonsAlpha.setEnabled(show);
         mDimNavButtonsAnimate.setEnabled(show);
         mDimNavButtonsAnimateDuration.setEnabled(show);
-    }
-
-	@Override
-	protected int getMetricsCategory() {
-        return MetricsLogger.APPLICATION;
     }
 
     @Override
