8c52651f30e9b80b11a56ae2829bc87ad6594c33
liquid0624 <rjbirnesser@gmail.com>
2015-10-18 21:32:01 -0400

    Settings: implement mallow tweaks base
    Adds mallowrom logo to about phone screen
    Custom temp mallow tweaks vectorized icon
    
    Conflicts:
    	res/values/mallow_strings.xml

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -97,6 +97,7 @@ import com.android.settings.inputmethod.KeyboardLayoutPickerFragment;
 import com.android.settings.inputmethod.SpellCheckersSettings;
 import com.android.settings.inputmethod.UserDictionaryList;
 import com.android.settings.location.LocationSettings;
+import com.android.settings.mallow.MallowTweaks;
 import com.android.settings.nfc.AndroidBeam;
 import com.android.settings.nfc.PaymentSettings;
 import com.android.settings.notification.AppNotificationSettings;
@@ -279,7 +280,8 @@ public class SettingsActivity extends Activity
             R.id.print_settings,
             R.id.nfc_payment_settings,
             R.id.home_settings,
-            R.id.dashboard
+            R.id.dashboard,
+            R.id.mallow_tweaks
     };
 
     private static final String[] ENTRY_FRAGMENTS = {
@@ -355,6 +357,7 @@ public class SettingsActivity extends Activity
             ProcessStatsSummary.class.getName(),
             DrawOverlayDetails.class.getName(),
             WriteSettingsDetails.class.getName(),
+            MallowTweaks.class.getName()
     };
 
 
