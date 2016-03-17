eac029225d791172da47567b3eedfb818a24705d
liquid0624 <rjbirnesser@gmail.com>
2016-03-17 02:59:12 -0400

    Revert "Settings: show gcc version and flags info"
    This reverts commit 7c3aa3880a8701c70709c92552063eeeece0a46e.

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -78,8 +78,6 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
     private static final String KEY_DEVICE_FEEDBACK = "device_feedback";
     private static final String KEY_MALLOW_VERSION = "mallow_version";
-    private static final String KEY_SM_AND = "sm_android";
-    private static final String KEY_SM_FLAGS = "sm_flags";
 
     /* Reduced taps from 7 to 3 for developer options */
     static final int TAPS_TO_BE_A_DEVELOPER = 3;
@@ -130,10 +128,6 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
         setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
         setValueSummary(KEY_MALLOW_VERSION, "ro.mallow.version");
-        findPreference(KEY_SM_AND).setEnabled(true);
-        findPreference(KEY_SM_FLAGS).setEnabled(true);
-        setValueSummary(KEY_SM_AND, "ro.sm.android");
-        setValueSummary(KEY_SM_FLAGS, "ro.sm.flags");
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);
