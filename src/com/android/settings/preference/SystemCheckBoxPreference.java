2f8c4d168261e93503d6fd27fecd066394bc06e9
maxwen <max.weninger@gmail.com>
2016-10-01 17:00:06 +0200

    Settings: bring back custom preference classes
    Change-Id: I91804774e4e46f0affb8f05c1a1566e17119acc8

--- src/com/android/settings/preference/SystemCheckBoxPreference.java
+++ src/com/android/settings/preference/SystemCheckBoxPreference.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,20 +17,20 @@
 package com.android.settings.preference;
 
 import android.content.Context;
-import android.support.v14.preference.SwitchPreference;
+import android.preference.CheckBoxPreference;
 import android.provider.Settings;
 import android.util.AttributeSet;
 
-public class SystemSettingSwitchPreference extends SwitchPreference {
-    public SystemSettingSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
+public class SystemCheckBoxPreference extends CheckBoxPreference {
+    public SystemCheckBoxPreference(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
     }
 
-    public SystemSettingSwitchPreference(Context context, AttributeSet attrs) {
+    public SystemCheckBoxPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public SystemSettingSwitchPreference(Context context) {
+    public SystemCheckBoxPreference(Context context) {
         super(context, null);
     }
 
@@ -41,6 +41,7 @@ public class SystemSettingSwitchPreference extends SwitchPreference {
                 // It's already there, so the same as persisting
                 return true;
             }
+
             Settings.System.putInt(getContext().getContentResolver(), getKey(), value ? 1 : 0);
             return true;
         }
@@ -52,6 +53,7 @@ public class SystemSettingSwitchPreference extends SwitchPreference {
         if (!shouldPersist()) {
             return defaultReturnValue;
         }
+
         return Settings.System.getInt(getContext().getContentResolver(),
                 getKey(), defaultReturnValue ? 1 : 0) != 0;
     }
